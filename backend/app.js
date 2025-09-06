379e96dfca254a66b895886209d370d7323db55e
Richard Gabelman <78068360+RichardGabelman@users.noreply.github.com>
2025-09-06 11:36:35 -0700

    feat: add basic express server

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res) => {
+  res.status(404).json({ error: "Not found" });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).json({ error: err.message });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Where's Waldo API - listening on port ${PORT}!`);
+});
